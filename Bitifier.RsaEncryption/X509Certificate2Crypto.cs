b6f0ff19bc461677660e9e1672c418356866f01c
Martin Knafve <knafve@gmail.com>
2015-12-25 20:31:25 +0100

    The RSACryptoServiceProvider should not be disposed.

--- Bitifier.RsaEncryption/X509Certificate2Crypto.cs
+++ Bitifier.RsaEncryption/X509Certificate2Crypto.cs
@@ -26,34 +26,32 @@ namespace Bitifier.RsaEncryption
             throw new ArgumentException("Certificate does not have a public key.", nameof(certificate), ex);
          }
 
-         using (RSACryptoServiceProvider publicKeyProvider =
-               (RSACryptoServiceProvider) certificate.PublicKey.Key)
+         RSACryptoServiceProvider publicKeyProvider = (RSACryptoServiceProvider) certificate.PublicKey.Key;
+
+         var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+
+         var maxDataLength = CalculateMaxDataLength(certificate);
+
+         var cipherText = new StringBuilder();
+
+         var chunks = plainTextBytes.Split(maxDataLength);
+
+         // Encrypting an empty string should still return ciphertext
+         if (chunks.Count == 0)
+            chunks.Add(new byte[0]);
+
+         for (int i = 0; i < chunks.Count; i++)
          {
-            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            if (i > 0)
+               cipherText.Append("|");
 
-            var maxDataLength = CalculateMaxDataLength(certificate);
+            var chunk = chunks[i];
 
-            var cipherText = new StringBuilder();
-
-            var chunks = plainTextBytes.Split(maxDataLength);
-
-            // Encrypting an empty string should still return ciphertext
-            if (chunks.Count == 0)
-               chunks.Add(new byte[0]);
-
-            for (int i = 0; i < chunks.Count; i++)
-            {
-               if (i > 0)
-                  cipherText.Append("|");
-
-               var chunk = chunks[i];
-
-               var cipherTextBytes = publicKeyProvider.Encrypt(chunk, true);
-               cipherText.Append(Convert.ToBase64String(cipherTextBytes, Base64FormattingOptions.None));
-            }
-
-            return cipherText.ToString();
+            var cipherTextBytes = publicKeyProvider.Encrypt(chunk, true);
+            cipherText.Append(Convert.ToBase64String(cipherTextBytes, Base64FormattingOptions.None));
          }
+
+         return cipherText.ToString();
       }
 
       public string Decrypt(X509Certificate2 certificate, string cipherText)
@@ -70,17 +68,15 @@ namespace Bitifier.RsaEncryption
 
          var plainText = new StringBuilder();
 
-         using (RSACryptoServiceProvider privateKeyProvider =
-            (RSACryptoServiceProvider) certificate.PrivateKey)
-         {
-            foreach (var chunk in chunks)
-            {
-               var cipherTextBytes = Convert.FromBase64String(chunk);
-               
-               var plainTextBytes = privateKeyProvider.Decrypt(cipherTextBytes, true);
+         RSACryptoServiceProvider privateKeyProvider = (RSACryptoServiceProvider) certificate.PrivateKey;
 
-               plainText.Append(Encoding.UTF8.GetString(plainTextBytes));
-            }
+         foreach (var chunk in chunks)
+         {
+            var cipherTextBytes = Convert.FromBase64String(chunk);
+               
+            var plainTextBytes = privateKeyProvider.Decrypt(cipherTextBytes, true);
+
+            plainText.Append(Encoding.UTF8.GetString(plainTextBytes));
          }
 
          return plainText.ToString();
