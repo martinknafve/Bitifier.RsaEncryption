639604bee9861b97d3af012269722cf196e54ad9
Martin Knafve <knafve@gmail.com>
2016-07-08 10:15:25 +0200

    Should be possible to perform encryption by suppliying a certificate file rather than a thumbprint.

--- Bitifier.RsaEncryption.Console/Program.cs
+++ Bitifier.RsaEncryption.Console/Program.cs
@@ -14,6 +14,7 @@ namespace Bitifier.RsaEncryption.Console
          string thumbprint = null;
          string inputFile = null;
          string outputFile = null;
+         string certificateFile = null;
 
          bool encrypt = false;
          bool decrypt = false;
@@ -24,6 +25,7 @@ namespace Bitifier.RsaEncryption.Console
                { "inputFile=",       v => inputFile = v },
                { "outputFile=",      v => outputFile = v },
                { "thumbprint=",      v => thumbprint = v },
+               { "certificateFile=", v => certificateFile = v },
                { "encrypt",          v => encrypt = true  },
                { "decrypt",          v => decrypt = true  },
                { "overwrite",        v => overwrite = true  },
@@ -35,8 +37,19 @@ namespace Bitifier.RsaEncryption.Console
 
             var errors = new StringBuilder();
 
-            if (string.IsNullOrWhiteSpace(thumbprint))
-               errors.AppendLine("Thumbprint must be specified.");
+            if (encrypt)
+            {
+               if (string.IsNullOrWhiteSpace(thumbprint) && string.IsNullOrWhiteSpace(certificateFile))
+                  errors.AppendLine("Either /thumbprint or /certificateFile must be used.");
+               if (!string.IsNullOrWhiteSpace(thumbprint) && !string.IsNullOrWhiteSpace(certificateFile))
+                  errors.AppendLine("Both /thumbprint or /certificateFile were used. Only one may be used at a time.");
+            }
+            else
+            {
+               if (!string.IsNullOrWhiteSpace(thumbprint) && !string.IsNullOrWhiteSpace(certificateFile))
+                  errors.AppendLine("When decrypting, a certificate must not be specified. It will be looked up from the thumbprint info in encrypted file.");
+            }
+
             if (string.IsNullOrWhiteSpace(inputFile))
                errors.AppendLine("Input file must be specified.");
             if (string.IsNullOrWhiteSpace(outputFile))
@@ -71,52 +84,77 @@ namespace Bitifier.RsaEncryption.Console
             return -1;
          }
 
-         StoreLocation storeLocation = StoreLocation.CurrentUser;
-         StoreName storeName = StoreName.My;
+         X509Certificate2 encryptionCertificate = null;
+         StoreLocation encryptionCertificateStoreLocation = StoreLocation.CurrentUser;
+         StoreName encryptionCertificateStoreName = StoreName.My;
 
-         System.Console.WriteLine(@"Attempting to find certificate in <CurrentUser>\Personal\Certificates...");
-
-         var certificateStore = new WindowsCertificateStoreRepository();
-         var matchingCertificates = certificateStore.Find(storeLocation, storeName, thumbprint);
-         var matchingCertificate = matchingCertificates.FirstOrDefault();
-
-         if (matchingCertificate == null)
+         if (encrypt)
          {
-            System.Console.WriteLine(@"Attempting to find certificate in <LocalMachine>\Personal\Certificates...");
+            if (!string.IsNullOrWhiteSpace(certificateFile))
+            {
+               if (!File.Exists(certificateFile))
+               {
+                  System.Console.WriteLine("The certificate file {0} was not found.", certificateFile);
+                  return -1;
+               }
 
-            storeLocation = StoreLocation.LocalMachine;
-            storeName = StoreName.My;
+               encryptionCertificate = new X509Certificate2(certificateFile);
+            }
 
-            matchingCertificates = certificateStore.Find(storeLocation, storeName, thumbprint);
-            matchingCertificate = matchingCertificates.FirstOrDefault();
-         }
 
-         if (matchingCertificate == null)
-         {
-            System.Console.WriteLine(
-               "Unable to find certificate with thumbprint {0}. Make sure it's imported into the Windows Certificate Store.",
-               thumbprint);
-            return -1;
-         }
-         else
-         {
+
+            if (encryptionCertificate == null)
+            {
+               System.Console.WriteLine(@"Attempting to find certificate in <CurrentUser>\Personal\Certificates...");
+
+               var certificateStore = new WindowsCertificateStoreRepository();
+               var matchingCertificates = certificateStore.Find(encryptionCertificateStoreLocation, encryptionCertificateStoreName, thumbprint);
+               encryptionCertificate = matchingCertificates.FirstOrDefault();
+
+               if (encryptionCertificate == null)
+               {
+                  System.Console.WriteLine(@"Attempting to find certificate in <LocalMachine>\Personal\Certificates...");
+
+                  encryptionCertificateStoreLocation = StoreLocation.LocalMachine;
+                  encryptionCertificateStoreName = StoreName.My;
+
+                  matchingCertificates = certificateStore.Find(encryptionCertificateStoreLocation, encryptionCertificateStoreName, thumbprint);
+                  encryptionCertificate = matchingCertificates.FirstOrDefault();
+               }
+            }
+
+            if (encryptionCertificate == null)
+            {
+               System.Console.WriteLine(
+                  "Unable to find certificate with thumbprint {0}. Make sure it's imported into the Windows Certificate Store.",
+                  thumbprint);
+               return -1;
+            }
+
             System.Console.WriteLine("Certificate found.");
          }
 
-      
-         if (File.Exists(outputFile) && !overwrite)
+         if (File.Exists(outputFile) && overwrite)
          {
             File.Delete(outputFile);
          }
 
          var inputFileContent = File.ReadAllText(inputFile, Encoding.UTF8);
-
-         var crypto = new X509Certificate2ThumbprintCrypto();
+         
          var serializer = new CipherTextWithCertificateInfoSerializer();
 
          if (encrypt)
          {
-            var cipherTextWithCertInfo = crypto.Encrypt(storeLocation, storeName, thumbprint, inputFileContent);
+            var crypto = new X509Certificate2Crypto();
+            var cipherText = crypto.Encrypt(encryptionCertificate, inputFileContent);
+
+            var cipherTextWithCertInfo = new CipherTextWithCertificateInfo()
+               {
+                  CipherText = cipherText,
+                  StoreLocation = encryptionCertificateStoreLocation,
+                  StoreName = encryptionCertificateStoreName,
+                  Thumbprint = encryptionCertificate.Thumbprint
+               };
 
             var serializedInfo = serializer.Serialize(cipherTextWithCertInfo);
             File.WriteAllText(outputFile, serializedInfo, Encoding.UTF8);
@@ -126,7 +164,8 @@ namespace Bitifier.RsaEncryption.Console
          else
          {
             var cipherTextWithCertInfo = serializer.Deserialize(inputFileContent);
-
+            
+            var crypto = new X509Certificate2ThumbprintCrypto();
             var plainText = crypto.Decrypt(cipherTextWithCertInfo);
 
             File.WriteAllText(outputFile, plainText, Encoding.UTF8);
